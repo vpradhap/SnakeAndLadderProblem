64e01fb8dfca23064d09fde0424349213cdd4331
vpradhap <pradhapvenkat@gmail.com>
2022-05-06 21:31:46 +0530

    Refractored : Repeating Loop until Player Position Reaches 100

--- Program.cs
+++ Program.cs
@@ -11,44 +11,48 @@ namespace SnakeAndLadderProblem
             Console.WriteLine("Player Start Position : " + position);
             //creating instance
             Random random = new Random();
-            //Generating Random Dieroll Number 1 to 6
-            dieroll = random.Next(1, 7);
-            Console.WriteLine("Dieroll Number is : " + dieroll);
-            //Generating Random  player choice Number 0 to 2
-            choice = random.Next(0, 3);
-            switch (choice)
+            //Repeating Loop until position Reaches 100 
+            while(position <= 100)
             {
-                case 0:
-                    Console.WriteLine("Player Option is : No Play");
-                    break;
-                case 1:
-                    Console.WriteLine("Player Option is : Ladder");
-                    break;
-                case 2:
-                    Console.WriteLine("Player Option is : Snake");
-                    break;
-            }
-            //operation based on choice 
-            if (choice == 1)
-            {
-                position += dieroll;
-            }
-            else if(choice == 2)
-            {
-                if (position < dieroll)
+                //Generating Random Dieroll Number 1 to 6
+                dieroll = random.Next(1, 7);
+                Console.WriteLine("Dieroll Number is : " + dieroll);
+                //Generating Random  player choice Number 0 to 2
+                choice = random.Next(0, 3);
+                switch (choice)
                 {
-                    position = 0;
+                    case 0:
+                        Console.WriteLine("Player Option is : No Play");
+                        break;
+                    case 1:
+                        Console.WriteLine("Player Option is : Ladder");
+                        break;
+                    case 2:
+                        Console.WriteLine("Player Option is : Snake");
+                        break;
+                }
+                //operation based on choice 
+                if (choice == 1)
+                {
+                    position += dieroll;
+                }
+                else if (choice == 2)
+                {
+                    if (position < dieroll)
+                    {
+                        position = 0;
+                    }
+                    else
+                    {
+                        position -= dieroll;
+                    }
                 }
                 else
                 {
-                    position -= dieroll;               
+                    position = position;
                 }
+                Console.WriteLine("Player Current Position : " + position);
             }
-            else
-            {
-                position = position;
-            }
-            Console.WriteLine("Player Current Position : "+position);
         }
     }
 }
