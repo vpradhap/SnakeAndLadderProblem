2ce4c4a7c5adb052f7813edadf44ccf4b3f236e4
vpradhap <pradhapvenkat@gmail.com>
2022-05-06 16:06:34 +0530

    Added : Die Rolled Number Using Random

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
 
-int position = 0;
+int position = 0,dieroll;
 Console.WriteLine("Player Start Position : "+position);
+Random random = new Random();
+dieroll = random.Next(1,7);
+Console.WriteLine("Dieroll Number is : "+dieroll);
